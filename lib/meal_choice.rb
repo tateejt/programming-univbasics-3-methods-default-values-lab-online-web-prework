0b97a4563941975dcdf5b1b12264da9edd4dfeab
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-24 03:57:52 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,6 @@
 # Your code here
 def meal_choice(veg1, veg2, protein ="meat")
-  p "What a nutritious meal!"
-  p "A plate of #{protein} with #{veg1} and #{veg2}."
+  p "What a nutritious meal!"&&"A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
 meal_choice("green","blue","meat")
